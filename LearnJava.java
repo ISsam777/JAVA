544a4b0f3606e333c472af63927d2a1443ade56b
ISsam777 <issam392004@gmail.com>
2025-03-23 22:10:40 +0100

    add

---
+++ LearnJava.java
@@ -0,0 +1,10 @@
+public class LearnJava {
+	public static void main(String[] args) {
+		int sum = MathUtils.sum(3, 2);
+		int div = MathUtils.div(4, 2);
+		int product = MathUtils.product(4, 2);
+		System.out.println(sum);
+		System.out.println(product);
+		System.out.println(div);
+	}
+}
---
+++ MathUtils.java
@@ -0,0 +1,13 @@
+public class MathUtils {
+	public static int sum(int a, int b) {
+		return a + b;
+	}
+
+	public static int product(int a, int b) {
+		return a * b;
+	}
+
+	public static int div(int a, int b) {
+		return a / b;
+	}
+}
